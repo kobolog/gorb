c38b82eba12afd8ac62c0a7c5dc84b8cbee71aad
Andrey Sibiryov <me@kobology.ru>
2015-11-07 00:25:53 -0500

    [General] Gofmt-ed the LICENSE.

--- core/context.go
+++ core/context.go
@@ -1,21 +1,21 @@
 /*
-    Copyright (c) 2015 Andrey Sibiryov <me@kobology.ru>
-    Copyright (c) 2015 Other contributors as noted in the AUTHORS file.
+   Copyright (c) 2015 Andrey Sibiryov <me@kobology.ru>
+   Copyright (c) 2015 Other contributors as noted in the AUTHORS file.
 
-    This file is part of GORB - Go Routing and Balancing.
+   This file is part of GORB - Go Routing and Balancing.
 
-    GORB is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+   GORB is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
 
-    GORB is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU Lesser General Public License for more details.
+   GORB is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 package core
