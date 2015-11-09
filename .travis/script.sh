e5f37c40245d3c101085240b3c7e2680ef67f7f7
Andrey Sibiryov <me@kobology.ru>
2015-11-09 11:01:57 -0500

    [Test] Support for Codecov, part 3.

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-OUTPUT=>(echo >> coverage.txt)
-
 set -e
 
+echo "" > coverage.txt
+
 for pkg in $@; do
-    go test -coverprofile=$OUTPUT -covermode=atomic ./$pkg
+    go test -coverprofile=profile.out -covermode=atomic ./$pkg
+
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm  profile.out
+    fi
 done
