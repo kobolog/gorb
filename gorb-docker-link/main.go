82fccfdaeb5bab6e9571ee72c38a14bf534832eb
BrianAdams <brian@babastik.com>
2015-12-10 01:07:20 -0800

    fixed #14; fixed service name munge for swarm

--- gorb-docker-link/main.go
+++ gorb-docker-link/main.go
@@ -183,7 +183,14 @@ func invokeFunc(vs, rs string, ports []gdc.APIPort, fn portAction) []error {
 		}, vs), binding.PrivatePort, binding.Type)
 
 		// Mangle the RS name.
-		rsID := fmt.Sprintf("%s-%d-%s", rs, binding.PrivatePort, binding.Type)
+		rsID := fmt.Sprintf("%s-%d-%s", strings.Map(func(r rune) rune {
+				switch r {
+				case '/', ':':
+				        return '-'
+				default:
+				        return r
+				}
+		},rs), binding.PrivatePort, binding.Type)
 
 		if err := fn(vsID, rsID, binding); err == nil {
 			n++
